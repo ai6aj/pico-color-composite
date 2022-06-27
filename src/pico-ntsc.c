3e6a15c3cd8ad176b5384a21b21f534a69b62568
Jon Suite <jon.suite@gmail.com>
2022-06-27 00:37:00 -0700

    Added RGB->NTSC color conversion, more cleanup

--- src/pico-ntsc.c
+++ src/pico-ntsc.c
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 // #include "pico/stdlib.h"
 #include "hardware/pio.h"
@@ -19,9 +20,140 @@
 #warning blink example requires a board with a regular LED
 #endif
 
-// No longer works due to change in PIO routine
-// Need to calculate new #s	
-// #define USE_FIXED_OVERCLOCK	0
+/* The Atari 8-bit palette, used to test RGB to NTSC palette conversion */
+
+uint32_t atari_8bit_fullColors[] = {
+0x00000000, 0x00202020,
+0x00404040, 0x00565656,
+0x006c6c6c, 0x007e7e7e,
+0x00909090, 0x00a0a0a0,
+0x00b0b0b0, 0x00bcbcbc,
+0x00c8c8c8, 0x00d2d2d2,
+0x00dcdcdc, 0x00e4e4e4,
+0x00ececec, 0x00ececec,
+0x00444400, 0x00545408,
+0x00646410, 0x0074741a,
+0x00848424, 0x0092922c,
+0x00a0a034, 0x00acac3a,
+0x00b8b840, 0x00c4c448,
+0x00d0d050, 0x00dcdc56,
+0x00e8e85c, 0x00f2f262,
+0x00fcfc68, 0x00fcfc68,
+0x00702800, 0x007a360a,
+0x00844414, 0x008e501e,
+0x00985c28, 0x00a26a32,
+0x00ac783c, 0x00b48244,
+0x00bc8c4c, 0x00c49654,
+0x00cca05c, 0x00d4aa62,
+0x00dcb468, 0x00e4be70,
+0x00ecc878, 0x00ecc878,
+0x00841800, 0x008e260c,
+0x00983418, 0x00a24224,
+0x00ac5030, 0x00b65c3c,
+0x00c06848, 0x00c87452,
+0x00d0805c, 0x00d88a66,
+0x00e09470, 0x00e69e78,
+0x00eca880, 0x00f4b28a,
+0x00fcbc94, 0x00fcbc94,
+0x00880000, 0x00921010,
+0x009c2020, 0x00a62e2e,
+0x00b03c3c, 0x00b84a4a,
+0x00c05858, 0x00c86464,
+0x00d07070, 0x00d87c7c,
+0x00e08888, 0x00e69494,
+0x00eca0a0, 0x00f4aaaa,
+0x00fcb4b4, 0x00fcb4b4,
+0x0078005c, 0x00821068,
+0x008c2074, 0x00962e7e,
+0x00a03c88, 0x00a84a92,
+0x00b0589c, 0x00b864a6,
+0x00c070b0, 0x00c87ab8,
+0x00d084c0, 0x00d690c8,
+0x00dc9cd0, 0x00e4a6d8,
+0x00ecb0e0, 0x00ecb0e0,
+0x00480078, 0x00541084,
+0x00602090, 0x006c2e9a,
+0x00783ca4, 0x00824aae,
+0x008c58b8, 0x009664c2,
+0x00a070cc, 0x00aa7ad4,
+0x00b484dc, 0x00bc90e4,
+0x00c49cec, 0x00cca6f4,
+0x00d4b0fc, 0x00d4b0fc,
+0x00140084, 0x0022108e,
+0x00302098, 0x003e2ea2,
+0x004c3cac, 0x005a4ab6,
+0x006858c0, 0x007264c8,
+0x007c70d0, 0x00887cd8,
+0x009488e0, 0x009e94e6,
+0x00a8a0ec, 0x00b2aaf4,
+0x00bcb4fc, 0x00bcb4fc,
+0x00000088, 0x000e1092,
+0x001c209c, 0x002a30a6,
+0x003840b0, 0x00444eb8,
+0x00505cc0, 0x005c68c8,
+0x006874d0, 0x007280d8,
+0x007c8ce0, 0x008698e6,
+0x0090a4ec, 0x009aaef4,
+0x00a4b8fc, 0x00a4b8fc,
+0x0000187c, 0x000e2886,
+0x001c3890, 0x002a469c,
+0x003854a8, 0x004462b2,
+0x005070bc, 0x005c7cc4,
+0x006888cc, 0x007292d4,
+0x007c9cdc, 0x0086a8e4,
+0x0090b4ec, 0x009abef4,
+0x00a4c8fc, 0x00a4c8fc,
+0x00002c5c, 0x000e3c6a,
+0x001c4c78, 0x002a5a84,
+0x00386890, 0x0044769e,
+0x005084ac, 0x005c90b6,
+0x00689cc0, 0x0072a8ca,
+0x007cb4d4, 0x0086c0de,
+0x0090cce8, 0x009ad6f2,
+0x00a4e0fc, 0x00a4e0fc,
+0x00003c2c, 0x000e4c3a,
+0x001c5c48, 0x002a6c56,
+0x00387c64, 0x00448c72,
+0x00509c80, 0x005ca88a,
+0x0068b494, 0x0072c2a0,
+0x007cd0ac, 0x0086dab6,
+0x0090e4c0, 0x009af0ca,
+0x00a4fcd4, 0x00a4fcd4,
+0x00003c00, 0x00104c10,
+0x00205c20, 0x00306c30,
+0x00407c40, 0x004e8c4e,
+0x005c9c5c, 0x0068a868,
+0x0074b474, 0x0080c280,
+0x008cd08c, 0x0098da98,
+0x00a4e4a4, 0x00aef0ae,
+0x00b8fcb8, 0x00b8fcb8,
+0x00143800, 0x00244a0e,
+0x00345c1c, 0x00426c2a,
+0x00507c38, 0x005e8a44,
+0x006c9850, 0x0078a65c,
+0x0084b468, 0x0090c072,
+0x009ccc7c, 0x00a8d886,
+0x00b4e490, 0x00bef09a,
+0x00c8fca4, 0x00c8fca4,
+0x002c3000, 0x003c400e,
+0x004c501c, 0x005a6028,
+0x00687034, 0x00767e40,
+0x00848c4c, 0x00909a58,
+0x009ca864, 0x00a8b46e,
+0x00b4c078, 0x00c0ca80,
+0x00ccd488, 0x00d6e092,
+0x00e0ec9c, 0x00e0ec9c,
+0x00442800, 0x0054380c,
+0x00644818, 0x00745824,
+0x00846830, 0x0092763a,
+0x00a08444, 0x00ac904e,
+0x00b89c58, 0x00c4a862,
+0x00d0b46c, 0x00dcc074,
+0x00e8cc7c, 0x00f2d684,
+0x00fce08c, 0x00fce08c
+};
+
+
 
 const uint LED_PIN = PICO_DEFAULT_LED_PIN;
 
@@ -31,14 +163,12 @@ int irq_count = 0;
 
 void video_core();
 
-// #define SYS_CLOCK_KHZ	150000
+// This overclocks the Pico a bit but gives us a nice
+// multiple of the NTSC clock frequency with low jitter
+// for a better signal.  Set this to 120000 for the default
+// Pico frequency.
 #define SYS_CLOCK_KHZ	157500
 
-// The amount of time to display a black screen
-// before turning on the video display.  This 
-// is needed to give the TV time to lock the 
-// VBLANK signal.
-#define STARTUP_FRAME_DELAY 120
 
 /**********************************
  FRAMEBUFFER STUFF
@@ -97,6 +227,11 @@ volatile int in_vblank = 0;
 #define VIDEO_START			(COLOR_BURST_START+SAMPLES_PER_CLOCK*30-2)
 #define VIDEO_LENGTH		192*SAMPLES_PER_CLOCK
 
+// The amount of time to display a black screen
+// before turning on the video display.  This 
+// is needed to give the TV time to lock the 
+// VBLANK signal.
+#define STARTUP_FRAME_DELAY 150
 
 /*
 	Set the total line width, in color clocks.
@@ -115,11 +250,79 @@ volatile int in_vblank = 0;
 #define LINE_WIDTH (226*SAMPLES_PER_CLOCK)
 #endif 
 
-void setPalette(int num,float a,float b,float c,float d) {
+void setPaletteRaw(int num,float a,float b,float c,float d) {
 	palette[num][0] = BLACK_LEVEL+(uint8_t)(a*LUMA_SCALE);
 	palette[num][1] = BLACK_LEVEL+(uint8_t)(b*LUMA_SCALE);
 	palette[num][2] = BLACK_LEVEL+(uint8_t)(c*LUMA_SCALE);
 	palette[num][3] = BLACK_LEVEL+(uint8_t)(d*LUMA_SCALE);
+}
+
+#define XXBLACK_LEVEL 0
+
+/**
+	Generate a palette entry from an NTSC phase/intensity/luma
+	triplet.  Given the complexity of NTSC encoding it's highly recommended
+	to use setPaletteRGB instead.
+	
+	chroma_phase		Phase of the chroma signal with respect to colorburst
+	chroma_amplitude	Amplitude of the chroma signal
+	luminance			The black and white portion of the signal
+	
+*/
+void setPaletteNTSC(int num,float chroma_phase,float chroma_amplitude,float luminance) {
+		
+	// Hue = phase 
+	float sat_scaled = LUMA_SCALE * chroma_amplitude;
+	
+	// Saturation = amplitude of chroma signal.
+	
+	int tmp = BLACK_LEVEL + sin(chroma_phase)*sat_scaled + luminance*LUMA_SCALE;
+	tmp = tmp < BLACK_LEVEL ? BLACK_LEVEL : tmp;
+	tmp = tmp > WHITE_LEVEL ? WHITE_LEVEL : tmp;
+	
+	
+	palette[num][0] = tmp;
+	tmp = BLACK_LEVEL + sin(chroma_phase+3.14159/2)*sat_scaled + luminance*LUMA_SCALE;
+	tmp = tmp < BLACK_LEVEL ? BLACK_LEVEL : tmp;
+	tmp = tmp > WHITE_LEVEL ? WHITE_LEVEL : tmp;
+	
+	palette[num][1] = tmp;
+	
+	tmp = BLACK_LEVEL + sin(chroma_phase+3.14159)*sat_scaled + luminance*LUMA_SCALE;
+	tmp = tmp < BLACK_LEVEL ? BLACK_LEVEL : tmp;
+	tmp = tmp > WHITE_LEVEL ? WHITE_LEVEL : tmp;
+	palette[num][2] = tmp;
+	
+	tmp = BLACK_LEVEL + sin(chroma_phase+3.14159*3/2)*sat_scaled + luminance*LUMA_SCALE;
+	tmp = tmp < BLACK_LEVEL ? BLACK_LEVEL : tmp;
+	tmp = tmp > WHITE_LEVEL ? WHITE_LEVEL : tmp;palette[num][3] = tmp;
+	palette[num][3] = tmp;
+}
+
+void setPaletteRGB_float(int num,float r, float g, float b) {
+	// Calculate Y 
+	float y = 0.299*r + 0.587*g + 0.114*b;
+
+	// Determine (U,V)
+	float u = 0.492111 * (b-y);
+	float v = 0.877283 * (r-y);
+
+	// Find S and H
+	float s = sqrt(u*u+v*v);
+	float h = atan2(v,u) + (55/180 * 3.14159 * 2);
+	if (h < 0) h += 2*3.14159;
+	
+//	h += (55/180 * 3.14159);
+	// Use setPalletteHSL to set the palette
+	setPaletteNTSC(num,h,s,y);
+}
+
+
+void setPaletteRGB(int num,uint8_t r, uint8_t g, uint8_t b) {
+	float rf = (float)r/255.0;
+	float gf = (float)g/255.0;
+	float bf = (float)b/255.0;
+	setPaletteRGB_float(num,rf,gf,bf);
 }
 
 int main() {
@@ -128,14 +331,11 @@ int main() {
     stdio_init_all();
 
 	multicore_launch_core1(video_core);
-
-	setPalette(0,0,0,0,0);
-	setPalette(1,1,1,0,0);
-	setPalette(2,1,0.1,0.1,0.1);
-	setPalette(3,1,1,1,1);
 	
 	for (int i=0; i<200; i++) {
-		memset(&framebuffer[i][0],0,160);
+		uint32_t rgb = atari_8bit_fullColors[i];
+		setPaletteRGB(i,(rgb & 0xFF0000) >> 16, (rgb & 0xFF00) >> 8, (rgb & 0xFF));
+		memset(&framebuffer[i][0],i,160);
 	}
 	
 	memset(&framebuffer[100][0],2,160);
@@ -146,15 +346,21 @@ int main() {
 	}
 
 	uint8_t tmp;
+	
+	uint8_t foo = 0;
+	
 	while(1) {
-//		printf("Hello, world.\n");
-		sleep_ms(500);
+		// Dump some output to USB serial to make sure it a. works at selected clock frequency
+		// and b. doesn't interfere with the display
+		printf("Hello, world.\n");
+		// Don't update the palette until VBLANK
 		while (!in_vblank);
-		tmp = palette[2][0];
-		palette[2][0] = palette[2][1];
-		palette[2][1] = palette[2][2];
-		palette[2][2] = palette[2][3];
-		palette[2][3] = tmp;
+		setPaletteRGB(2,0,0,foo);
+		foo++;
+
+		// Wait until we're out of VBLANK before proceeding
+		while (in_vblank);
+
 	}
 }
 
@@ -395,27 +601,11 @@ static uint8_t* __not_in_flash_func(user_render_ex)(uint line) {
 
 int frcount = 0;
 
-#define CCC	2
 static void __not_in_flash_func(user_render)(uint line, uint8_t* dest,user_render_func_t user_func) {
-	//	uint8_t* nextline = (line & 1) ? pingpong_lines[0] : pingpong_lines[1];
 		uint8_t* sourceline = user_func(line);
 		uint8_t* colorptr;
 		frcount++;
 		int ofs = VIDEO_START;
-
-/*
-		for (int i=0; i<160; i++) {
-			// colorptr = palette[sourceline[i]];
-		
-			// The compiler will optimize this		
-			dest[ofs] = 96 + i;
-			dest[ofs+1] = 96 + i;
-			dest[ofs+2] = 96 + i;
-			dest[ofs+3] = 96 + i;
-			ofs += 4;
-		}
-*/
-
 
 		for (int i=0; i<160; i++) {
 			colorptr = palette[sourceline[i]];
@@ -453,19 +643,6 @@ static void __not_in_flash_func(make_video_line)(uint line, uint8_t* dest) {
 			ofs += 4;
 		}
 }
-
-
-
-/*
-
-DAC test only
-
-static void __not_in_flash_func(cvideo_dma_handler)(void) {
-    dma_channel_set_read_addr(dma_channel, test_line, true);
-    dma_hw->ints0 = 1 << dma_channel;		
-}
-
-*/
 
 static void __not_in_flash_func(cvideo_dma_handler)(void) {
 	
@@ -626,7 +803,6 @@ void start_video(PIO pio, uint sm, uint offset, uint pin, uint num_pins) {
      if (div_int != 0) {
          div_frac = (uint8_t)((PIO_clkdiv - (float)div_int) * (1u << 8u));
      }
-//	pio->sm[sm].clkdiv = (uint32_t) (PIO_clkdiv * (1 << 16)); // INT portion: 0xffff0000, FRAC portion: 0x0000ff00	
 
 	pio->sm[sm].clkdiv = (((uint)div_frac) << PIO_SM0_CLKDIV_FRAC_LSB) |
              (((uint)div_int) << PIO_SM0_CLKDIV_INT_LSB);
