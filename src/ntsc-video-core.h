d641fa0b27a515f23a169476e3bd455e47eca977
Jon Suite <jon.suite@gmail.com>
2022-09-02 00:08:36 -0700

    Cleaned up ntsc-video-core.h file.

--- src/ntsc-video-core.h
+++ src/ntsc-video-core.h
@@ -22,34 +22,16 @@ void ntsc_video_core();
 
 float get_video_core_load();
 
-// This overclocks the Pico a bit but gives us a nice
-// multiple of the NTSC clock frequency with low jitter
-// for a better signal.  Set this to 133000 for the maximum
-// RP2040 frequency that's still in spec; most emulation 
-// should still work but display quality will suffer.
-//
-// The *default* Pico frequency is 120000, at this frequency
-// you can generate display but some emulators (i.e. Atari 8-bit)
-// won't have enough CPU time to generate their worst-case
-// display.
-//
-// #define SYS_CLOCK_KHZ	120000
-// #define SYS_CLOCK_KHZ	133000
-//#define SYS_CLOCK_KHZ	157500
-#define SYS_CLOCK_KHZ	150000
+// This is almost an ideal multiple of the NTSC
+// colorburst signal, which gives us very low jitter.
+// Some devices (i.e. my cheap projector) will refuse
+// to display in color if you go below this value.
+#define SYS_CLOCK_KHZ	157500
+
+// 195 or 204MHz are (almost) good multiples for PAL.
+// #define SYS_CLOCK_KHZ	204000
 
 // The phase of the generated colorburst signal.  
-// Old video hardware had a pot you could adjust to tune the
-// color.  This emulates that pot.  Adjust it to get the 
-// correct(ish) color output for your emulator; NTSC displays
-// were notoriously inaccurate and so were old consoles so
-// don't expect to ever get perfect color rendition... there
-// was no such thing.
-//
-// TODO this should be a default; the value should be 
-// adjustable in software.  To properly implement it the
-// video core will need to rebuild the colorburst signal
-// during VBLANK.
 #define COLOR_BURST_PHASE_DEGREES 180.0
 
 /**********************************
@@ -75,39 +57,66 @@ extern volatile int in_vblank;
 #define MAX_DAC_OUT	((1 << DAC_BITS)-1)
 #define MIN_DAC_OUT	0
 
+#ifndef USE_PAL
 #define USE_NTSC
+#endif
 
-#ifdef USE_PAL
+#ifdef USE_NTSC
+	/* NTSC values. */
+
+	#define BLANKING_VAL		(uint)(((40.0/100.0)*(float)MAX_DAC_OUT)+0.5)
+	#define COLOR_BURST_HI_VAL	(uint)(((60.0/140.0)*(float)MAX_DAC_OUT)+0.5)
+	#define COLOR_BURST_LO_VAL	(uint)(((20.0/140.0)*(float)MAX_DAC_OUT)+0.5)
+	#define SYNC_VAL			MIN_DAC_OUT
+	#define BLACK_LEVEL			(uint)(((47.5/140.0)*(float)MAX_DAC_OUT)+0.5)
+	#define WHITE_LEVEL			MAX_DAC_OUT
+	#define LUMA_SCALE			(WHITE_LEVEL-BLACK_LEVEL)
+
+	/* 
+		Values needed to make timing calculations
+	*/
+
+	#define NTSC_COLORBURST_MHZ		(3.5795454)
+	#define NTSC_COLORBURST_FREQ	(NTSC_COLORBURST_MHZ*1000000)
+	#define CLOCK_FREQ (float)(SAMPLES_PER_CLOCK*NTSC_COLORBURST_MHZ)
+	#define SAMPLE_LENGTH_US	(1.0/CLOCK_FREQ)
+
+	#define LINE_WIDTH (226*SAMPLES_PER_CLOCK)
+
+	/*
+		Various timings needed to generate a proper NTSC signal.
+	*/
+	#define SYNC_TIP_CLOCKS 	(int)(4.7/(SAMPLE_LENGTH_US)+0.5)
+	#define COLOR_BURST_START	(int)(5.3/(SAMPLE_LENGTH_US)+0.4)
+
+	#define VBLANK_CLOCKS		(int)(27.1/(SAMPLE_LENGTH_US)+0.5)
 	
-	/* PAL values. 
+	// VIDEO_START *MUST* be 32-bit aligned.
+	#define VIDEO_START			(SAMPLES_PER_CLOCK*39)
+	#define VIDEO_LENGTH		192*SAMPLES_PER_CLOCK
 	
-		These values are not to spec, but they're the only ones my cheap 
-		NTSC/PAL to USB converter will lock onto properly - the proper 
-		ones cause the entire video display to be shifted 50% to the right.  Unfortunately
-		I don't have any real PAL equipment to test with, so they'll 
-		have to do for now.
+	#define LINES_PER_FRAME			262
+	#define COLORBURST_FREQ	NTSC_COLORBURST_FREQ
+	#define VIDEO_START_PHASE_SHIFT	0
+
+
+#else // PAL
+	
+	/*  PAL values. 
+	
+		These colorburst frequency is not to spec, but it's the only ones 
+		that my cheap component-to-USB converter and LCD projector will accept
+		produce a color display.  The colors are incorrect, however.
 		
-		It may be possible to sync PAL by extending the VBLANK line by 50%.
+		My TV interprets PAL timings as component (YPbPr) input, yielding 
+		a black and white display.
 		
-		-Jon	
-		
-		
-		NOTES:
-		
-		http://martin.hinner.info/vga/pal.html
-		
-		Look under 'Timing', the paragraph starting 
-		"You can stop the display being interlaced if you want"
-		Seems like with proper implementation of "pre-equalization"
-		and "long sync" pulses we can avoid doing a half-line.
 	*/
 
 	#define BLANKING_VAL		(uint)(((45.0/143.0)*(float)MAX_DAC_OUT)+0.5)
 	#define COLOR_BURST_HI_VAL	(uint)(((55.0/143.0)*(float)MAX_DAC_OUT)+0.5)
 	#define COLOR_BURST_LO_VAL	(uint)(((35.0/143.0)*(float)MAX_DAC_OUT)+0.5)
 
-//	#define COLOR_BURST_HI_VAL	(uint)(((43.0/100.0)*(float)MAX_DAC_OUT)+0.5)
-//	#define COLOR_BURST_LO_VAL	(uint)(((14.0/100.0)*(float)MAX_DAC_OUT)+0.5)
 	#define SYNC_VAL			MIN_DAC_OUT
 	#define BLACK_LEVEL			(uint)(((45.0/143.0)*(float)MAX_DAC_OUT)+0.5)
 	#define WHITE_LEVEL			MAX_DAC_OUT
@@ -121,7 +130,7 @@ extern volatile int in_vblank;
 	// PAL colorburst timing is much more finicky than NTSC for some reason.
 	// The fraction below adjusts the colorburst frequency so it works on
 	// my (admittedly bad) equipment.
-	#define PAL_COLORBURST_MHZ		(4.43361875 * 180.54/180.0)
+	#define PAL_COLORBURST_MHZ		(4.43361875 * 180.25/180.0)
 	#define PAL_COLORBURST_FREQ	(PAL_COLORBURST_MHZ*1000000)
 	#define CLOCK_FREQ (float)(SAMPLES_PER_CLOCK*PAL_COLORBURST_MHZ)
 	#define SAMPLE_LENGTH_US	(1.0/CLOCK_FREQ)
@@ -156,57 +165,8 @@ extern volatile int in_vblank;
 	#define COLORBURST_FREQ	PAL_COLORBURST_FREQ
 	#define VIDEO_START_PHASE_SHIFT	0
 
-#else // NTSC
-
-	/* NTSC values. */
-
-	#define BLANKING_VAL		(uint)(((40.0/100.0)*(float)MAX_DAC_OUT)+0.5)
-	#define COLOR_BURST_HI_VAL	(uint)(((60.0/140.0)*(float)MAX_DAC_OUT)+0.5)
-	#define COLOR_BURST_LO_VAL	(uint)(((20.0/140.0)*(float)MAX_DAC_OUT)+0.5)
-	#define SYNC_VAL			MIN_DAC_OUT
-	#define BLACK_LEVEL			(uint)(((47.5/140.0)*(float)MAX_DAC_OUT)+0.5)
-	#define WHITE_LEVEL			MAX_DAC_OUT
-	#define LUMA_SCALE			(WHITE_LEVEL-BLACK_LEVEL)
-
-	/* 
-		Values needed to make timing calculations
-	*/
-
-	#define NTSC_COLORBURST_MHZ		(3.5795454)
-	#define NTSC_COLORBURST_FREQ	(NTSC_COLORBURST_MHZ*1000000)
-	#define CLOCK_FREQ (float)(SAMPLES_PER_CLOCK*NTSC_COLORBURST_MHZ)
-	#define SAMPLE_LENGTH_US	(1.0/CLOCK_FREQ)
-
-	/*
-		Set the total line width, in color clocks.
-		ALTERNATE_COLORBURST_PHASE will generate proper 262.5
-		color clock lines but is only partially supported at
-		the moment (and doesn't seem to be necessary.)	
-	*/
-
-	#ifdef ALTERNATE_COLORBURST_PHASE
-		#define LINE_WIDTH (227*SAMPLES_PER_CLOCK-(SAMPLES_PER_CLOCK/2))
-	#else 
-		#define LINE_WIDTH (226*SAMPLES_PER_CLOCK)
-	#endif 
-
-	/*
-		Various timings needed to generate a proper NTSC signal.
-	*/
-	#define SYNC_TIP_CLOCKS 	(int)(4.7/(SAMPLE_LENGTH_US)+0.5)
-	#define COLOR_BURST_START	(int)(5.3/(SAMPLE_LENGTH_US)+0.4)
-
-	#define VBLANK_CLOCKS		(int)(27.1/(SAMPLE_LENGTH_US)+0.5)
-	
-	// VIDEO_START *MUST* be 32-bit aligned.
-	#define VIDEO_START			(SAMPLES_PER_CLOCK*50)
-	#define VIDEO_LENGTH		192*SAMPLES_PER_CLOCK
-	
-	#define LINES_PER_FRAME			262
-	#define COLORBURST_FREQ	NTSC_COLORBURST_FREQ
-	#define VIDEO_START_PHASE_SHIFT	0
-
 #endif
+
 // The chroma phase shift needed to adjust for VIDEO_START.  This
 // will typically be some fraction of pi.
 
